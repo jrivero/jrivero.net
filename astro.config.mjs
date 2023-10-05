c396fe94049364948e01ff61b58eb42800dc721d
Jordi Rivero <jrivero@leadtech.com>
2023-10-05 13:01:12 +0200

    added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import robots from "astro-robots";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jrivero.net',
-	integrations: [
-		mdx(),
-		sitemap(),
-	],
+  site: 'https://jrivero.net',
+  integrations: [mdx(), sitemap(), robots()]
 });
