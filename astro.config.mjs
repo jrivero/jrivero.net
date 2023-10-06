c8c82f31a1c56729d844d7a8dbf748883613eddf
Jordi Rivero <jrivero@leadtech.com>
2023-10-06 10:59:07 +0200

    removed mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+// import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import robots from "astro-robots";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://jrivero.net',
   trailingSlash: 'never',
-  integrations: [mdx(), sitemap(), robots()]
+  integrations: [sitemap(), robots()]
 });
