706c7b41226557fca294a89b7e185094989ff046
Jordi Rivero <jrivero@leadtech.com>
2023-11-26 11:48:59 +0100

    added keystatic and netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-// import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import robots from "astro-robots";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://jrivero.net',
   trailingSlash: 'never',
-  integrations: [sitemap(), robots()]
+  integrations: [sitemap(), robots(), react(), markdoc(), keystatic()],
+  output: 'hybrid',
+  adapter: netlify()
 });
---
+++ keystatic.config.ts
@@ -0,0 +1,29 @@
+// keystatic.config.ts
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  ui: {
+    brand: { name: 'Jordi Blog' }
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
+  },
+});
