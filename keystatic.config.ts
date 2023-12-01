762564a003a476946bca2d31afe48b4d8da86a86
Jordi Rivero <jrivero@leadtech.com>
2023-12-01 16:03:10 +0100

    testing keystatic

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -75,5 +75,35 @@ export default config({
         }),
       },
     }),
+    categories: collection({
+      label: "Categories",
+      slugField: "name",
+      path: "src/content/categories/*",
+      schema: {
+        name: fields.slug({ name: { label: "Name" } }),
+        intro: fields.text({ label: "Intro", multiline: true }),
+      },
+    }),
+    topics: collection({
+      label: "Topics",
+      slugField: "title",
+      path: "src/content/topics/*",
+      format: { contentField: "content" },
+      entryLayout: "content",
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        category: fields.relationship({
+          label: "Category",
+          collection: "categories",
+        }),
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
   },
 });
