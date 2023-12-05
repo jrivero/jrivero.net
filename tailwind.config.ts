5d29d3492e71adaa83512228f65db2b64c081229
Jordi Rivero <jrivero@leadtech.com>
2023-12-05 14:30:08 +0100

    playing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,14 @@ export default {
       center: true,
       padding: '2rem',
       screens: {
-        '2xl': '1400px',
+        sm: '576px',
+        // => @media (min-width: 576px) { ... }
+
+        md: '960px',
+        // => @media (min-width: 960px) { ... }
+
+        lg: '1440px',
+        // => @media (min-width: 1440px) { ... }
       },
     },
     extend: {
