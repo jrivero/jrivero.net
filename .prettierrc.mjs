9f43746aa56b31ad98d1d6faa200cb853ec1f921
Jordi Rivero <jrivero@leadtech.com>
2024-02-22 19:39:10 +0100

    refactor

--- .prettierrc.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-  printWidth: 150,
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
-  endOfLine: 'lf',
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
-}
